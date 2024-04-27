bc5d1170b87228a57ffa296d3ce8b2856184e131
SergeiU <heartofkerrigan@gmail.com>
2024-04-27 12:44:10 +0900

    добавляет настройки babel

--- webpack.config.js
+++ webpack.config.js
@@ -14,4 +14,18 @@ module.exports = {
       patterns: [{from: 'public'}],
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presents: ['@babel/present-env']
+          },
+        },
+      },
+    ],
+  },
 };
